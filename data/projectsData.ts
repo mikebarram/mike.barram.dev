b3954a6a141062e9d5d96bb8b9e0dee7f6bc40d1
Mike Barram <mikebarram@users.noreply.github.com>
2022-09-28 23:09:42 +0100

    Initial commit

---
+++ data/projectsData.ts
@@ -0,0 +1,16 @@
+const projectsData = [
+  {
+    title: 'This site',
+    description: `How this site was developed using Next.js, React and Tailwind. Including  the migration of content from WordPress to Markdown`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://www./blog/this-site',
+  },
+  {
+    title: 'Mouse in a maze',
+    description: `A project to try out machine learning, using Python.`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/the-time-machine',
+  },
+]
+
+export default projectsData
