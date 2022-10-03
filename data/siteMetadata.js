266640ed9f22b828a8b41b5c254fd663a2af0514
Mike Barram <mikebarram@users.noreply.github.com>
2022-10-03 23:27:40 +0100

    Fixed my name

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 const siteMetadata = {
   title: 'Mike Barram',
   author: 'Mike Barram',
-  headerTitle: 'MikeBarram',
+  headerTitle: 'Mike Barram',
   description: "Mike Barram's work as a developer and architect",
   language: 'en-gb',
   theme: 'system', // system, dark or light
