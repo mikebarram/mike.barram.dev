fe3e72d2431e8d328098c6377c1e0f0583eb31fd
Mike Barram <mikebarram@users.noreply.github.com>
2022-10-04 20:47:09 +0100

    New blog post

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -2,14 +2,14 @@ const projectsData = [
   {
     title: 'This site',
     description: `How this site was developed using Next.js, React and Tailwind. Including  the migration of content from WordPress to Markdown`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www./blog/this-site',
+    imgSrc: '/static/images/lighthouse.png',
+    href: '/tags/this-site',
   },
   {
     title: 'Mouse in a maze',
     description: `A project to try out machine learning, using Python.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    imgSrc: '/static/images/non-ai-mouse-in-a-maze.png',
+    href: '/blog/2022/not-ai-mouse-in-a-maze',
   },
 ]
 
